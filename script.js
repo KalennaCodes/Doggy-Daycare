116efaf21b9f1828ee81d9fc6c89ef38f52c32bc
Kalenna Smith <kalenna.s@hotmail.com>
2022-03-28 15:58:38 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.getElementById("hamburger");
 const navUL = document.getElementById("nav-ul");
+const main = document.getElementById("mainHide")
 
 hamburger.addEventListener("click", () => {
     navUL.classList.toggle("show");
